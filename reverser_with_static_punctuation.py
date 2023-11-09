f006ed8b9c7e694de69c9d289fec9d0662741e71
benjaminsiegler <bensiegler@icloud.com>
2023-11-09 16:51:41 +0100

    got pointer adjustments working properly. testing with new cases.

--- reverser_with_static_punctuation.py
+++ reverser_with_static_punctuation.py
@@ -4,7 +4,14 @@ def reverser(s):
     first_word_end = first_word_start
     second_word_end = second_word_start + 1
 
-    while not (second_word_start - first_word_start) <= 1:
+    while True:
+        while not s[first_word_start].isalpha():
+            first_word_start += 1
+        first_word_end = first_word_start
+
+        while not s[second_word_start].isalpha():
+            second_word_start += 1
+        second_word_end = second_word_start
 
         while s[first_word_end].isalpha():
             first_word_end += 1
@@ -12,19 +19,22 @@ def reverser(s):
         while s[second_word_end].isalpha():
             second_word_end += 1
 
+        if (second_word_start - first_word_end) <= 1:
+            break
+
         first_word = s[first_word_start:first_word_end]
         second_word = s[second_word_start:second_word_end]
         index_adjustment = len(first_word) - len(second_word)
+        s = s[:second_word_start] + first_word + s[second_word_end:]
         s = s[:first_word_start] + second_word + s[first_word_end:]
-        s = s[:second_word_start] + first_word + s[second_word_end - index_adjustment:]
 
-        first_word_start = first_word_end
-        while not s[first_word_start].isalpha():
-            first_word_start += 1
+        first_word_start = first_word_end - index_adjustment
+        prev_second_word_start = second_word_start - index_adjustment
+        while second_word_start >= prev_second_word_start:
+            second_word_start = s.rfind(" ", None, second_word_start - 1) + 1
 
-        second_word_start = s.rfind(" ", None, second_word_start)
-        second_word_end = second_word_start
     return s
 
 
-print(reverser("hello, this is ben."))
+# print(reverser("hello, this is ben."))
+print(reverser("hi I am tired"))
